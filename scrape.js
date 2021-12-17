c14b1e47a7428d3d7fd38a2587d62f24ad32eb91
aaroncnc <81981239+aaroncnc@users.noreply.github.com>
2021-12-17 11:14:09 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,27 +1,30 @@
 const cron = require('node-cron');
 
-const puppeteer = require('puppeteer')
+//const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 var fs = require('fs')
 
 var arrayOfItems;
 
 // UPDATE WITH YOUR LOCATION REFERENCE FROM STEP 4
-let locationRef = 'sydney'
+let locationRef = 'change'
 
 // UPDATE WITH ITEMS YOU WANT TO SEARCH FOR
-let searchTerms = ['jeep', 'iphone']
+let searchTerms = ['cnc']
 
 const nodemailer = require('nodemailer');
 
 // UPDATE WITH EMAIL YOU WANT TO RECEIVE AT
-let emailRecipient = "recipient@email.com"
+let emailRecipient = "change"
 
 // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'sending@email.com',
-      pass: 'email_password' 
+      user: 'change@gmail.com',
+      pass: 'change' 
     }
   });
 
@@ -33,7 +36,7 @@ function sendEmail(emailRecipient, searchTerm, items){
         message = message + item_string
     }
   const mailOptions = {
-        from: '"Marketplace Alert" sending@email.com',
+        from: '"Marketplace Alert" change@gmail.com',
         to: emailRecipient,
         subject: `${items.length} new items listed under ${searchTerm}`,
         text: message
@@ -51,7 +54,7 @@ async function getItems(){
   fs.readFile('./pastItems.json', 'utf-8', function(err, data) {
   arrayOfItems = JSON.parse(data);
   })
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({headless: false})
   const page = await browser.newPage()
   for (var i=0;i<searchTerms.length;i++){
     var newItems = [];
@@ -62,25 +65,29 @@ async function getItems(){
     let searchResult = JSON.parse(bodyHTML.split(/(?:"marketplace_search":|,"marketplace_seo_page")+/)[2]);
     let items = searchResult["feed_units"]["edges"]
     if (items.length > 1){
+		try {
       items.forEach((val, index)=>{
-        var ID = val['node']['listing']['id'];
+        var ID1 = val['node']['listing']['id'];
         var link = `https://www.facebook.com/marketplace/item/${val['node']['listing']['id']}`;
         var title = val['node']['listing']['marketplace_listing_title'];
         var price = val['node']['listing']['listing_price']['formatted_amount'];
         var item = {title: title, price: price, link: link}
-        if (arrayOfItems.pastItems.includes(ID)){
+        if (arrayOfItems.pastItems.includes(ID1)){
         } else {
-          arrayOfItems.pastItems.push(ID)
+          arrayOfItems.pastItems.push(ID1)
           newItems.push(item);  
           console.log(item)
         } 
       });
-    }
-  if (newItems.length>0){
-    sendEmail(emailRecipient, searchTerms[i], newItems);
-  } else {
-    console.log('No new items for ' + searchTerms[i]);
+		} catch (err) {
+    console.error(err.message);
   }
+    }
+	if (newItems.length>0){
+		sendEmail(emailRecipient, searchTerms[i], newItems);
+	} else {
+		console.log('No new items for ' + searchTerms[i]);
+	}
   };
   await browser.close()
   fs.writeFile('./pastItems.json', JSON.stringify(arrayOfItems), 'utf-8', function(err) {
@@ -88,9 +95,10 @@ async function getItems(){
     console.log('Updated past items')
   })
 }
-
+getItems()
 // TO CHANGE CRON TIME SCHEDULE
 // https://www.npmjs.com/package/node-cron
 cron.schedule('*/10 * * * *', function() {
   getItems()
 });
+//getItems()
