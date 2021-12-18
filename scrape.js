892ca276fd1bfce6274a49b81987f852a8300b5e
aaroncnc <81981239+aaroncnc@users.noreply.github.com>
2021-12-17 20:18:06 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -12,12 +12,12 @@ var arrayOfItems;
 let locationRef = 'change'
 
 // UPDATE WITH ITEMS YOU WANT TO SEARCH FOR
-let searchTerms = ['cnc']
+let searchTerms = ['change', 'change']
 
 const nodemailer = require('nodemailer');
 
 // UPDATE WITH EMAIL YOU WANT TO RECEIVE AT
-let emailRecipient = "change"
+let emailRecipient = "change@gmail.com"
 
 // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
 const transporter = nodemailer.createTransport({
@@ -36,6 +36,7 @@ function sendEmail(emailRecipient, searchTerm, items){
         message = message + item_string
     }
   const mailOptions = {
+	  // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
         from: '"Marketplace Alert" change@gmail.com',
         to: emailRecipient,
         subject: `${items.length} new items listed under ${searchTerm}`,
@@ -60,15 +61,21 @@ async function getItems(){
     var newItems = [];
     var searchTerm = searchTerms[i].replace(/ /g,'%20');    
     console.log(`\nResults for ${searchTerms[i]}:\n`)
-	  await page.waitForTimeout(5000)
+	await page.waitForTimeout(5000)
     await page.goto(`https://www.facebook.com/marketplace/${locationRef}/search/?daysSinceListed=1&sortBy=best_match&query=${searchTerm}&exact=false`)
-	  await page.waitForTimeout(5000)
+	//console.log(`\n1\n`)
+	await page.waitForTimeout(5000)
     let bodyHTML = await page.evaluate(() => document.body.outerHTML);
+	//console.log(`\n2\n`)
+	await page.waitForTimeout(5000)
+	try {
     let searchResult = JSON.parse(bodyHTML.split(/(?:"marketplace_search":|,"marketplace_seo_page")+/)[2]);
+	//console.log(`\n3\n`)
+	await page.waitForTimeout(5000)
     let items = searchResult["feed_units"]["edges"]
-    await page.waitForTimeout(5000)
+	//console.log(`\n4\n`)
+	await page.waitForTimeout(5000)
     if (items.length > 1){
-		try {
       items.forEach((val, index)=>{
         var ID1 = val['node']['listing']['id'];
         var link = `https://www.facebook.com/marketplace/item/${val['node']['listing']['id']}`;
@@ -82,10 +89,10 @@ async function getItems(){
           console.log(item)
         } 
       });
-		} catch (err) {
+		} 
+    } catch (err) {
     console.error(err.message);
   }
-    }
 	if (newItems.length>0){
 		sendEmail(emailRecipient, searchTerms[i], newItems);
 	} else {
@@ -93,14 +100,15 @@ async function getItems(){
 	}
   };
   await browser.close()
+  //console.log(`\n5\n`)
   fs.writeFile('./pastItems.json', JSON.stringify(arrayOfItems), 'utf-8', function(err) {
     if (err) throw err
     console.log('Updated past items')
   })
 }
+
 // TO CHANGE CRON TIME SCHEDULE
 // https://www.npmjs.com/package/node-cron
 cron.schedule('*/10 * * * *', function() {
   getItems()
 });
-
